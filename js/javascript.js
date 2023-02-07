d1c9dd89d6832894a0db292de70dca55addf03ad
Sukant Sondhi <sukant.sondhi.2021@live.rhul.ac.uk>
2023-02-07 23:02:18 +0000

    Making website responsive

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+/* Typing Animation*/
+
+var typed = new Typed(".typing", {
+    strings: ["CS Major", "Backend Developer", "Athlete", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+
+
+
+
+
